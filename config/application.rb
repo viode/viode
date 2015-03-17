03ba7e00e15b4c08bdc06f55756f3719822e19b5
Nihad Abbasov <mail@narkoz.me>
2015-03-18 00:07:06 +0400

    disable some generators

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,11 @@ module Viode
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.view_specs false
+    end
   end
 end
