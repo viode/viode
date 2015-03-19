cfe29e5499ca1f3020564401db3ceb55757c0df0
Nihad Abbasov <mail@narkoz.me>
2015-03-19 16:59:32 +0400

    improve seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,10 @@ create :confirmed_user, email: 'user@example.com'
   create :category
 end
 
-User.all.each do |user|
-  Category.all.each do |category|
-    rand(2..5).times { create :question, user: user, category: category }
-  end
+Category.all.each do |category|
+  rand(2..5).times { create :question, user: User.all.sample, category: category }
+end
 
-  Question.all.each do |question|
-    rand(2..5).times { create :answer, user: user, question: question }
-  end
+Question.all.each do |question|
+  rand(0..3).times { create :answer, user: User.all.sample, question: question }
 end
