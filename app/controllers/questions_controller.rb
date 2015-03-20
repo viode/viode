6eedf7f7fe0a04eabcdf8a66a33d895a6cc1fc34
Nihad Abbasov <mail@narkoz.me>
2015-03-21 01:31:32 +0400

    add some styling to pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @questions = Question.includes(:category, :user).recent.limit(10)
+    @questions = Question.includes(:category, :user).recent.limit(20)
   end
 
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def time_tag_for(object)
+    time = object.created_at
+    time_tag time, "#{distance_of_time_in_words_to_now time} ago", title: l(time, format: :long)
+  end
 end
