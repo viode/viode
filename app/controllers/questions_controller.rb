a970c9550229d5be4d51564e52c1d431ad92d283
Nihad Abbasov <mail@narkoz.me>
2015-03-21 18:17:01 +0400

    list categories and labels on mainpage

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,8 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @categories = Category.all
+    @labels = ActsAsTaggableOn::Tag.most_used
     @questions = Question.includes(:category, :user).recent.page(params[:page]).per(20)
   end
 
