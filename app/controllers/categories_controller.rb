a86fd255a0f04899f156bb5f4df387349cb0907b
Nihad Abbasov <mail@narkoz.me>
2015-03-22 20:52:35 +0400

    add categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,8 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
   def show
     @category = Category.find params[:id]
   end
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe CategoriesController, type: :controller do
   let(:category) { create :category }
 
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
   describe "GET #show" do
     it "returns http success" do
       get :show, id: category.id
