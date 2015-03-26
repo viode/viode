440da187d9faead39f05883fe5b3ba22b5d855a7
Nihad Abbasov <mail@narkoz.me>
2015-03-26 16:07:13 +0400

    add specs for answer votes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  let(:user)   { create :confirmed_user }
+  let(:answer) { create :answer, author: user }
+
   describe "relations" do
     it { should belong_to(:author).class_name('User') }
     it { should belong_to(:question) }
@@ -8,5 +11,35 @@ RSpec.describe Answer, type: :model do
 
   describe "validations" do
     it { should validate_presence_of(:body) }
+  end
+
+  describe "has_reputation" do
+    it "has reputation upvotes" do
+      expect(answer.reputation_for(:upvotes)).to eq(0)
+    end
+
+    it "has reputation downvotes" do
+      expect(answer.reputation_for(:downvotes)).to eq(0)
+    end
+
+    it "has reputation votes" do
+      expect(answer.reputation_for(:votes)).to eq(0)
+    end
+
+    it "changes reputation for upvotes" do
+      answer.add_evaluation :upvotes, 1, user
+      expect(answer.reputation_for(:upvotes)).to eq(1)
+    end
+
+    it "changes reputation for downvotes" do
+      answer.add_evaluation :downvotes, -1, user
+      expect(answer.reputation_for(:downvotes)).to eq(-1)
+    end
+
+    it "changes reputation for votes" do
+      answer.add_evaluation :upvotes, 5, user
+      answer.add_evaluation :downvotes, -2, user
+      expect(answer.reputation_for(:votes)).to eq(3)
+    end
   end
 end
