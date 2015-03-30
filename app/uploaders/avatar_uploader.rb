ab0020c342d934f01e343906fd40101a5e450079
Nihad Abbasov <mail@narkoz.me>
2015-03-31 03:27:05 +0500

    remove default user avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,10 +12,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url
-    ActionController::Base.helpers.asset_path([version_name, 'avatar.png'].compact.join('_'))
-  end
-
   process :strip_data
   process resize_to_fill: [250, 250]
 
