e042eb43e1fb2a71aa284e98345837abda98ead6
Nihad Abbasov <mail@narkoz.me>
2015-04-01 00:47:38 +0500

    add dynamic page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,19 @@ module ApplicationHelper
       image_tag post.author.avatar.thumb.url, size: '18', alt: post.author.username
     end
   end
+
+  # TODO: refactor
+  def title(*args)
+    @title_helper_title ||= []
+    @title_helper_title_options ||= { separator: ' - ', headline: nil, sitename: nil }
+    options = args.extract_options!
+
+    @title_helper_title += args
+    @title_helper_title_options.merge!(options)
+
+    t =  @title_helper_title.clone
+    t << @title_helper_title_options[:sitename]
+    t << @title_helper_title_options[:headline]
+    t.compact.join(@title_helper_title_options[:separator])
+  end
 end
