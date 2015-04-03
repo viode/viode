41b7c946139d20dbf5671fbc7090048976c35b62
Nihad Abbasov <mail@narkoz.me>
2015-04-04 00:09:47 +0500

    show labels in search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
 class SearchController < ApplicationController
   def index
-    @questions = Question.search params[:query], page: params[:page], per_page: 10
+    if params[:query] && params[:query].size > 1
+      tags = ActsAsTaggableOn::Tag.arel_table
+      @tags = ActsAsTaggableOn::Tag.where tags[:name].matches("%#{params[:query]}%")
+      @questions = Question.search params[:query], page: params[:page], per_page: 10
+    else
+      params.delete(:query)
+      @tags = []
+      @questions = []
+    end
   end
 end
