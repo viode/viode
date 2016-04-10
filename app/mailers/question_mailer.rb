4c8f8ed0f57de94f4ef3d1ba219a51ec8c444bd3
rhuberdeau <rhuberdeau@gmail.com>
2016-04-10 14:41:10 -0400

    Adding the question mailer

---
+++ app/mailers/question_mailer.rb
@@ -0,0 +1,7 @@
+class QuestionMailer < ApplicationMailer
+  def specified_user(question)
+    @question = question
+    @user     = User.find(@question.intended_respondent)
+    mail(to: @user.email, subject: 'Someone asked you a question', content_type: "text/html")
+  end
+end
